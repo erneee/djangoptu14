b694669df56dfd636d665db42224190237f5eabe
Ernestas <ernestasvvv@gmail.com>
2023-08-24 15:55:39 +0300

    patobulinome book detail

--- library/urls.py
+++ library/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.index, name='index'),
     path('authors/', views.authors, name='authors-all'),
     path('authors/<int:author_id>', views.author, name='author-one'),
     path('books/', views.BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-one')
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-one'), ##books/<int:pk> pk yra primary_key
+
 ]
